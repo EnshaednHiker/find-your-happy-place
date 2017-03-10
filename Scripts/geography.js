50820c1326f1aca87f142bb78a02fb8c0d81fac2
EnshaednHiker <nbingham09@gmail.com>
2017-03-10 16:28:15 -0600

    started project, got some initial file structure set up

---
+++ Scripts/geography.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Admin on 3/9/2017.
+ */
+
+
+/* Create object factory that either takes a continent for an argument 
+and returns only locations from that continent or doesn't take any arguments and returns all locations
