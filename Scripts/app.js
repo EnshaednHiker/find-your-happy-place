b69d5242dcad4d006a839993e51feed76689a3f7
EnshaednHiker <nbingham09@gmail.com>
2017-03-14 18:02:33 -0500

    finished form event handler, so can now store form data

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,5 +1,32 @@
 /**
  * Created by Admin on 3/9/2017.
  */
-var newGeography = Geography ("Europe");
+
+const happyData = {
+            continent:"",
+            rangeBegin:"",
+            rangeEnd:"",
+            sunniness:""
+        }
+
+
+var newGeography ={};
 console.log(newGeography);
+
+
+
+
+ $("#landing-page-form" ).on("submit", function(event){
+        event.preventDefault();
+        
+        happyData.continent=$("select#continent-selection").find(':selected').data('continent');
+        happyData.rangeBegin=$("select#temp-selection").find(':selected' ).data('rangeBegin');
+        happyData.rangeEnd=$("select#temp-selection").find(':selected').data('rangeEnd');
+        happyData.sunniness=$("select#sunniness-selection").find(':selected').data('sunniness');
+        newGeography = Geography (happyData.continent);
+        console.log(newGeography);
+    });
+
+$("#js-start-over-button").click(function(event){
+    location.reload();
+});
